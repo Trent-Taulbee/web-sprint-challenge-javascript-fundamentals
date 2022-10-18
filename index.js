f98a2555863464e44531e46212b261eab5bafbe6
Trent Taulbee <taulbee.trent@gmail.com>
2022-10-18 00:26:13 -0400

    solved request 1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function myFunction() {
 //myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
+// Internal is being passed along further into the function just as your parents passed DNA down to you. Information can be passed down, but cannot be passed up.
 
 
 
@@ -30,11 +30,14 @@ function myFunction() {
 💡 NOTE: you may use a for loop for this function if you wish 
 */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  var counter = 0;
+  for (let i = 0; i <= number; i++){
+    counter += i;
   }
- 
+  return counter;
+  }
+ console.log('Task 2:',summation(10))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
@@ -60,10 +63,14 @@ const zooAnimals = [
   💡 NOTE: the array returned should be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(array){
+    const displayNames = [];
+    for (let i = 0; i < array.length; i++){
+    displayNames.push(`name: ${array[i].animal_name}, scientific: ${array[i].scientific_name}`);
+    }
+    return displayNames
   }
-  
+  console.log(animalNames(zooAnimals))
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoo needs a list of all their animal's names converted to lower case. 
